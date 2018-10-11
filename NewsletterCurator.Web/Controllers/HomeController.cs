9718f5e490034f8a542bb1646153dce6985e44ad
Christopher Demicoli <christopher.demicoli@outlook.com>
2018-10-11 22:54:27 +0200

    Showing categories just for test

--- NewsletterCurator.Web/Controllers/HomeController.cs
+++ NewsletterCurator.Web/Controllers/HomeController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using NewsletterCurator.Data;
 
 namespace NewsletterCurator.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly NewsletterCuratorContext newsletterCuratorContext;
+
+        public HomeController(NewsletterCuratorContext newsletterCuratorContext)
+        {
+            this.newsletterCuratorContext = newsletterCuratorContext;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(newsletterCuratorContext.Categories.ToList());
         }
     }
 }
