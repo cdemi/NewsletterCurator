2b55ef3809cabe96282a6acfe414fecbb8d46b63
Christopher Demicoli <christopher.demicoli@outlook.com>
2018-10-11 22:44:02 +0200

    Changed Seeded IDs to static

--- NewsletterCurator.Data/NewsletterCuratorContext.cs
+++ NewsletterCurator.Data/NewsletterCuratorContext.cs
@@ -17,11 +17,11 @@ namespace NewsletterCurator.Data
             modelBuilder.Entity<Category>(options =>
             {
                 options.HasData(
-                    new Category { ID = Guid.NewGuid(), Name = "DevOps" },
-                    new Category { ID = Guid.NewGuid(), Name = "Front End" },
-                    new Category { ID = Guid.NewGuid(), Name = "Security" },
-                    new Category { ID = Guid.NewGuid(), Name = "iGaming" },
-                    new Category { ID = Guid.NewGuid(), Name = ".NET" }
+                    new Category { ID = new Guid("bbf3205e-578b-4568-9d86-7c15fceb6a4f"), Name = "DevOps" },
+                    new Category { ID = new Guid("3f9acf3f-bf48-455d-9a3f-f660cd3a13b3"), Name = "Front End" },
+                    new Category { ID = new Guid("40e0baf7-3b80-4866-b9ae-3a2e77ad88fb"), Name = "Security" },
+                    new Category { ID = new Guid("44754987-6f3f-4b5e-a79d-a61b13a61647"), Name = "iGaming" },
+                    new Category { ID = new Guid("e17226a6-bed1-44f5-863f-3970bb634fce"), Name = ".NET" }
                         );
             });
         }
