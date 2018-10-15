28d31ce1e1856a9b84bb9ef536ff7ce1dc9eea73
Christopher Demicoli <christopher.demicoli@outlook.com>
2018-10-15 10:42:54 +0200

    Changed DateTime to DateTimeOffset

---
+++ NewsletterCurator.Data.SqlServer/Migrations/20181015084230_Changed DateTime to DateTimeOffset.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NewsletterCurator.Data.Migrations
+{
+    public partial class ChangedDateTimetoDateTimeOffset : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "DateTime",
+                table: "Newsitems",
+                nullable: false,
+                oldClrType: typeof(DateTime));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateTime",
+                table: "Newsitems",
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset));
+        }
+    }
+}
