5ec7208a97b1a92a349bbd79b72134930a465ba8
Christopher Demicoli <christopher.demicoli@outlook.com>
2018-10-15 17:33:34 +0200

    Initial Work on #15 and #5

---
+++ NewsletterCurator.Web/Controllers/EmailController.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NewsletterCurator.Data;
+using NewsletterCurator.Web.Models;
+
+namespace NewsletterCurator.Web.Controllers
+{
+    public class EmailController : Controller
+    {
+        private readonly NewsletterCuratorContext newsletterCuratorContext;
+
+        public EmailController(NewsletterCuratorContext newsletterCuratorContext)
+        {
+            this.newsletterCuratorContext = newsletterCuratorContext;
+        }
+
+        public async Task<IActionResult> Preview()
+        {
+            var categoryNewsItemsViewModels = await newsletterCuratorContext.NewsitemsByCategory().Select(c => new CategoryNewsItemsViewModel { Category = c.Key, Newsitems = c.ToList() }).ToListAsync();
+
+            return View(categoryNewsItemsViewModels);
+        }
+    }
+}
