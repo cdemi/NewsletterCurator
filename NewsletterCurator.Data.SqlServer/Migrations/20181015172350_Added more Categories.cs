a649c980d85874be3a0fc0f852193b53778c1867
Christopher Demicoli <christopher.demicoli@outlook.com>
2018-10-15 19:24:26 +0200

    Added more Categories

---
+++ NewsletterCurator.Data.SqlServer/Migrations/20181015172350_Added more Categories.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NewsletterCurator.Data.Migrations
+{
+    public partial class AddedmoreCategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "ID", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("57e0baf7-3b80-4866-b9ae-3a2e77ad88fb"), "AI" },
+                    { new Guid("12e0baf7-3b80-4866-b9ae-3a2e77ad88fb"), "Space" },
+                    { new Guid("527ff497-33d2-434c-a1db-5a722d94078f"), "Infrastructure" },
+                    { new Guid("317ff497-33d2-434c-a1db-5a722d94078f"), "Software Development" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: new Guid("12e0baf7-3b80-4866-b9ae-3a2e77ad88fb"));
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: new Guid("317ff497-33d2-434c-a1db-5a722d94078f"));
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: new Guid("527ff497-33d2-434c-a1db-5a722d94078f"));
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: new Guid("57e0baf7-3b80-4866-b9ae-3a2e77ad88fb"));
+        }
+    }
+}
--- NewsletterCurator.Data/NewsletterCuratorContext.cs
+++ NewsletterCurator.Data/NewsletterCuratorContext.cs
@@ -22,9 +22,13 @@ namespace NewsletterCurator.Data
                 options.HasData(
                     new Category { ID = new Guid("bbf3205e-578b-4568-9d86-7c15fceb6a4f"), Name = "DevOps" },
                     new Category { ID = new Guid("3f9acf3f-bf48-455d-9a3f-f660cd3a13b3"), Name = "Front End" },
+                    new Category { ID = new Guid("57e0baf7-3b80-4866-b9ae-3a2e77ad88fb"), Name = "AI" },
+                    new Category { ID = new Guid("12e0baf7-3b80-4866-b9ae-3a2e77ad88fb"), Name = "Space" },
                     new Category { ID = new Guid("40e0baf7-3b80-4866-b9ae-3a2e77ad88fb"), Name = "Security" },
                     new Category { ID = new Guid("44754987-6f3f-4b5e-a79d-a61b13a61647"), Name = "iGaming" },
                     new Category { ID = new Guid("497FF497-33D2-434C-A1DB-5A722D94078F"), Name = "General Tech" },
+                    new Category { ID = new Guid("527FF497-33D2-434C-A1DB-5A722D94078F"), Name = "Infrastructure" },
+                    new Category { ID = new Guid("317FF497-33D2-434C-A1DB-5A722D94078F"), Name = "Software Development" },
                     new Category { ID = new Guid("e17226a6-bed1-44f5-863f-3970bb634fce"), Name = ".NET" }
                         );
             });
