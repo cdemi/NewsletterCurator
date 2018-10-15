1cf0579200e4b032e9a27c9068d3095d70350896
Christopher Demicoli <christopher.demicoli@outlook.com>
2018-10-16 00:24:22 +0200

    Added eSports Category

---
+++ NewsletterCurator.Data.SqlServer/Migrations/20181015222411_Added eSports Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NewsletterCurator.Data.Migrations
+{
+    public partial class AddedeSportsCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "ID", "Name", "Priority" },
+                values: new object[] { new Guid("84754987-6f3f-4b5e-a79d-a61b13a61647"), "eSports", 8.5f });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: new Guid("84754987-6f3f-4b5e-a79d-a61b13a61647"));
+        }
+    }
+}
