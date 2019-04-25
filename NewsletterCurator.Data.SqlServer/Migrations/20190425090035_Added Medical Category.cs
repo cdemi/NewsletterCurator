48c8d2472d76e2022c8419c61f4b043d20809adb
Christopher Demicoli <christopher.demicoli@outlook.com>
2019-04-25 11:02:30 +0200

    Added Medical Category

---
+++ NewsletterCurator.Data.SqlServer/Migrations/20190425090035_Added Medical Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NewsletterCurator.Data.Migrations
+{
+    public partial class AddedMedicalCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "ID", "Color", "HashTag", "Name", "Priority" },
+                values: new object[] { new Guid("927af497-33a2-424c-a1db-5a722d92078a"), "#a74141", "Medical", "Medical", 89f });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: new Guid("927af497-33a2-424c-a1db-5a722d92078a"));
+        }
+    }
+}
