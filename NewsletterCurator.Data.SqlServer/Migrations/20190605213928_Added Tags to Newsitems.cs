3bad6fa68efb7f04957de788d8f33ef2999c3d45
Christopher Demicoli <christopher.demicoli@outlook.com>
2019-06-05 23:39:49 +0200

    Added Tags to Newsitems

---
+++ NewsletterCurator.Data.SqlServer/Migrations/20190605213928_Added Tags to Newsitems.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NewsletterCurator.Data.Migrations
+{
+    public partial class AddedTagstoNewsitems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Tags",
+                table: "Newsitems",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tags",
+                table: "Newsitems");
+        }
+    }
+}
