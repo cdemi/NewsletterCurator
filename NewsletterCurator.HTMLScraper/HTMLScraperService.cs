b497878dc2208c61c049cf0771ab1f5d1183bd12
Christopher Demicoli <christopher.demicoli@outlook.com>
2019-06-06 10:18:41 +0200

    Improved favicon detection

--- NewsletterCurator.HTMLScraper/HTMLScraperService.cs
+++ NewsletterCurator.HTMLScraper/HTMLScraperService.cs
@@ -95,7 +95,7 @@ namespace NewsletterCurator.HTMLScraper
 
             urlMetadata.Tags = urlMetadata.Tags.Distinct().ToList();
 
-            var faviconTag = htmlDoc.DocumentNode.SelectNodes("/html/head/link[contains(@rel, 'icon') and not(contains(@rel, '-icon'))]")?.FirstOrDefault();
+            var faviconTag = htmlDoc.DocumentNode.SelectNodes("//link[contains(@rel, 'icon') and not(contains(@rel, '-icon'))]")?.FirstOrDefault();
             if (faviconTag != null)
             {
                 urlMetadata.FaviconURL = faviconTag.GetAttributeValue("href", null);
