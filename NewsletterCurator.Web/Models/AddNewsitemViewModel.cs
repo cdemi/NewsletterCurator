d0f5fb260090ec11fb56ce60052f106526d64d2f
Christopher Demicoli <christopher.demicoli@outlook.com>
2019-06-10 10:32:57 +0200

    Added color to categories

--- NewsletterCurator.Web/Models/AddNewsitemViewModel.cs
+++ NewsletterCurator.Web/Models/AddNewsitemViewModel.cs
@@ -9,7 +9,7 @@ namespace NewsletterCurator.Web.Models
     {
         public string URL { get; set; }
         public Guid CategoryID { get; set; }
-        public List<SelectListItem> Categories { get; set; }
+        public List<Category> Categories { get; set; }
         public string Title { get; set; }
         public string ImageURL { get; set; }
         public List<string> Images { get; set; }
