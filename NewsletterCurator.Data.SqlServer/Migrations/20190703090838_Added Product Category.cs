9ed0bef9a096e77c2a0c0423b5774ffebe854c22
Christopher Demicoli <christopher.demicoli@outlook.com>
2019-07-03 11:08:45 +0200

    Added Product Category

---
+++ NewsletterCurator.Data.SqlServer/Migrations/20190703090838_Added Product Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NewsletterCurator.Data.Migrations
+{
+    public partial class AddedProductCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "ID", "Color", "HashTags", "Name", "Priority" },
+                values: new object[] { new Guid("12e0baf7-3b82-4866-b9ae-5a2e77ad88fb"), "#683864", "Product,ProductOwnership,ProductManagement", "Product", 5.9f });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: new Guid("12e0baf7-3b82-4866-b9ae-5a2e77ad88fb"));
+        }
+    }
+}
