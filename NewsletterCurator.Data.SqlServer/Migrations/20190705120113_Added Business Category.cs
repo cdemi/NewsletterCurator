928e36b8a505f428ac7bb048e55640fcaa753445
Christopher Demicoli <christopher.demicoli@outlook.com>
2019-07-05 14:01:40 +0200

    Added Business Category

---
+++ NewsletterCurator.Data.SqlServer/Migrations/20190705120113_Added Business Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NewsletterCurator.Data.Migrations
+{
+    public partial class AddedBusinessCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "ID", "Color", "HashTags", "Name", "Priority" },
+                values: new object[] { new Guid("91e0baf7-3c82-4866-b9ae-5a2e77ad88fa"), "#57106e", "Business", "Business", 5.85f });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: new Guid("91e0baf7-3c82-4866-b9ae-5a2e77ad88fa"));
+        }
+    }
+}
