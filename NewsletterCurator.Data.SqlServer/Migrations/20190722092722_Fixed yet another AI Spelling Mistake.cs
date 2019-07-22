32cf34ccbde2104aa49afd3f4b08fd83cf00fe89
Christopher Demicoli <christopher.demicoli@outlook.com>
2019-07-22 11:27:38 +0200

    Fixed yet another AI Spelling Mistake

---
+++ NewsletterCurator.Data.SqlServer/Migrations/20190722092722_Fixed yet another AI Spelling Mistake.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NewsletterCurator.Data.Migrations
+{
+    public partial class FixedyetanotherAISpellingMistake : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: new Guid("57e0baf7-3b80-4866-b9ae-3a2e77ad88fb"),
+                column: "Name",
+                value: "Artificial Intelligence");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: new Guid("57e0baf7-3b80-4866-b9ae-3a2e77ad88fb"),
+                column: "Name",
+                value: "Artifical Intelligence");
+        }
+    }
+}
