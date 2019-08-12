299ff3bcc8d1429eeb74dd17cea02f8160d34cc8
Christopher Demicoli <christopher.demicoli@outlook.com>
2019-08-12 16:16:28 +0200

    Added more hashtags to categories

---
+++ NewsletterCurator.Data.SqlServer/Migrations/20190812141544_Added more hashtags to categories.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NewsletterCurator.Data.Migrations
+{
+    public partial class Addedmorehashtagstocategories : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: new Guid("317ff497-33d2-434c-a1db-5a722d94078f"),
+                column: "HashTags",
+                value: "SoftwareDevelopment,Software,Dev,Programming");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: new Guid("622fa497-32d2-434c-a1db-5a722d94012f"),
+                column: "HashTags",
+                value: "IoT,InternetOfThings");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: new Guid("317ff497-33d2-434c-a1db-5a722d94078f"),
+                column: "HashTags",
+                value: "SoftwareDevelopment,Software");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: new Guid("622fa497-32d2-434c-a1db-5a722d94012f"),
+                column: "HashTags",
+                value: "IoT,InternetofThings");
+        }
+    }
+}
