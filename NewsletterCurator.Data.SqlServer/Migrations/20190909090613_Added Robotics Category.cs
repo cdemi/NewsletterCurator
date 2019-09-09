e64c244eb7563d9f31233e1ff269306ec959b785
Christopher Demicoli <christopher.demicoli@outlook.com>
2019-09-09 11:07:34 +0200

    Added Robotics Category

---
+++ NewsletterCurator.Data.SqlServer/Migrations/20190909090613_Added Robotics Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NewsletterCurator.Data.Migrations
+{
+    public partial class AddedRoboticsCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "ID", "Color", "HashTags", "Name", "Priority" },
+                values: new object[] { new Guid("67e0baf7-3b80-4866-b9ae-3a2e77ad88fb"), "#388f5b", "Robotics", "Robotics", 6.9f });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "ID",
+                keyValue: new Guid("67e0baf7-3b80-4866-b9ae-3a2e77ad88fb"));
+        }
+    }
+}
